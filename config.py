7f569fc9b1ccf05b617582f5a171f4f89e46b52f
Swapniel1 <swapnielr@gmail.com>
2026-04-29 02:55:07 +0600

    config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Bot Configuration File
+
+BOT_TOKEN = "8567983293:AAFwRwa3AF0S2IwJcEK8J3COKBrkGqEYRwg"
+ADMIN_ID = 7743673373  # এখানে আপনার Telegram User ID দিন (Miss Rose বট থেকে পাবেন)
+
+# Force Join Channel (বটকে অবশ্যই এই চ্যানেলের Admin বানাতে হবে)
+FORCE_JOIN_CHANNEL = "@lol23730" 
+
+# Reward Settings
+REF_REWARD = 5.0      # প্রতি রেফারে কত টাকা
+DAILY_BONUS = 2.0     # ডেইলি বোনাস
+MIN_WITHDRAW = 50.0   # সর্বনিম্ন উইথড্র লিমিট
+
+# Currency Symbol
+CURRENCY = "৳"
